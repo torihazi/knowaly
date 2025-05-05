d01d6237712cca5c25efdd8300219fe36502f2d0
torihazi <lovebarusa19981225@icloud.com>
2025-05-05 17:03:27 +0900

    comment out middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
-import { type NextRequest } from "next/server";
-import { updateSession } from "@/lib/supabase/middleware";
+// import { type NextRequest } from "next/server";
+// import { updateSession } from "@/lib/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+// export async function middleware(_request: NextRequest) {
+export async function middleware() {
+  // return await updateSession(request);
 }
 
 export const config = {
