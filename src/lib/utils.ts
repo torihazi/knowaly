c3ef519a575ca135de739624c0bf10d7fbda5922
torihazi <lovebarusa19981225@icloud.com>
2025-05-10 12:06:39 +0900

    create

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const handleEnterKeyDown = (
+  e: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>,
+  callback: () => void
+) => {
+  if (e.key === "Enter" && !e.shiftKey && !e.nativeEvent.isComposing) {
+    e.preventDefault();
+    callback();
+  }
+};
